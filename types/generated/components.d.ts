8d2cab8166357d7ea4b0b1e523c7d806e90326bc
RobArkesteijn <rarkesteijn@outlook.com>
2024-06-22 15:04:44 +0200

    Migrate to strapi v5

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,49 +1,49 @@
-import type { Schema, Attribute } from '@strapi/strapi';
+import type { Struct, Schema } from '@strapi/strapi';
 
-export interface CountryAttractions extends Schema.Component {
-  collectionName: 'components_country_attractions';
+export interface HomeHiglightedCountries extends Struct.ComponentSchema {
+  collectionName: 'components_home_higlighted_countries';
   info: {
-    displayName: 'Attractions';
-    description: '';
+    displayName: 'HiglightedCountries';
   };
   attributes: {
-    attraction: Attribute.String & Attribute.Required;
-    description: Attribute.Text;
-    image: Attribute.Media;
+    country: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
   };
 }
 
-export interface HomeHighlighted extends Schema.Component {
+export interface HomeHighlighted extends Struct.ComponentSchema {
   collectionName: 'components_home_highlighteds';
   info: {
     displayName: 'Highlighted';
     description: '';
   };
   attributes: {
-    headline: Attribute.String;
-    title: Attribute.String & Attribute.Required;
-    description: Attribute.RichText;
-    countries: Attribute.Component<'home.higlighted-countries', true>;
+    headline: Schema.Attribute.String;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+    description: Schema.Attribute.RichText;
+    countries: Schema.Attribute.Component<'home.higlighted-countries', true>;
   };
 }
 
-export interface HomeHiglightedCountries extends Schema.Component {
-  collectionName: 'components_home_higlighted_countries';
+export interface CountryAttractions extends Struct.ComponentSchema {
+  collectionName: 'components_country_attractions';
   info: {
-    displayName: 'HiglightedCountries';
+    displayName: 'Attractions';
+    description: '';
   };
   attributes: {
-    country: Attribute.String;
-    image: Attribute.Media;
+    attraction: Schema.Attribute.String & Schema.Attribute.Required;
+    description: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images'>;
   };
 }
 
-declare module '@strapi/types' {
-  export module Shared {
-    export interface Components {
-      'country.attractions': CountryAttractions;
-      'home.highlighted': HomeHighlighted;
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
       'home.higlighted-countries': HomeHiglightedCountries;
+      'home.highlighted': HomeHighlighted;
+      'country.attractions': CountryAttractions;
     }
   }
 }
